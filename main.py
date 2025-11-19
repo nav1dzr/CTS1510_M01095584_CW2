aaf3198db0aea5d6236747dfa6e12585b8a6fb04
Navid Zarei <Navidz7111>
2025-11-19 12:00:54 +0000

    updated

--- main.py
+++ main.py
@@ -69,5 +69,32 @@ def main():
         else:
             print("\nError: Invalid option. Please select 1, 2, or 3.")
 
-if name == "__main__":
-    main()
+
+
+
+import sqlite3
+conn = sqlite3.connect('DATA/telligence_platform.db')
+
+
+def add_user(conn,name,hash):
+    curr = conn.cursor()
+    sql = ("""INSERT INTO users (username, password_hash) VALUES (?, ?) """)
+    param = (name,hash) 
+    curr.execute(sql,param)
+    conn.commit()
+
+
+def get_user():
+    curr = conn.cursor()
+    sql = ("""SELECT * FROM users""")
+    curr.execute(sql)
+    users = curr.fetchall()
+    return users
+
+def migrate_user_data():
+    with open("DATA/users.txt", "r") as f:
+        users = f.readlines()
+    for user in users:
+       name, hash = user.strip().split(",")
+       add_user(conn,name,hash)
+    conn.close()
